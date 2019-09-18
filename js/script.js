1769c525059e9f53ae8ac6ccebaf4f5cfe9d115d
Suboch Denis <suboch.denny@gmail.com>
2019-09-18 12:54:24 +0300

    created search-field, added js file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let searchButton = document.querySelector('.search-link_clicked');
+searchButton.addEventListener('click', function() {
+    alert(2);
+    let searchFormContainer =  document.querySelector('.search-form-comtainer');
+    searchFormContainer.classList.add(.);
+});
