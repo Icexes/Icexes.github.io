cdb77a310b021321f125eb87f27c3a30fe4f2c9c
Denis <suboch.denny@gmail.com>
2019-12-12 18:14:52 +0300

    worked  on hiding menu

--- js/script.js
+++ js/script.js
@@ -4,12 +4,29 @@ searchButton.addEventListener('click', function () {
     toggle(searchButton, 'search-link--clicked')
     toggle(searchFormContainer, 'search-form-container--show')
 });
-document.querySelector(".mobile-menu__button").addEventListener("click", function (event) {
+
+document.addEventListener("click", function(event) {
+    if (event.target.closest(".mobile-menu__button")) {
+        toggle(".mobile-menu", "mobile-menu__visible")
+        toggle(".mobile-menu__button", "mobile-menu__open")
+        toggle(".navbar__logo", "mobile-menu__open")
+        toggle("body", "constrained")
+        return;
+    }
+    if (document.querySelector(".mobile-menu").classList.contains("mobile-menu__visible")) {
+    if (event.target.closest(".mobile-menu")) {
+        return;
+        
+    }
     toggle(".mobile-menu", "mobile-menu__visible")
     toggle(".mobile-menu__button", "mobile-menu__open")
     toggle(".navbar__logo", "mobile-menu__open")
     toggle("body", "constrained")
-});
+    }
+
+}) 
+
+
 function toggle(selector, className) {
     let findedSelector = typeof selector == 'object' ? selector : document.querySelector(selector);
     findedSelector.classList.toggle(className);
