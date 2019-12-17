58fa4ea9cac0047142fac7543861e3222093e045
Denis <suboch.denny@gmail.com>
2019-12-17 10:21:26 +0300

    renamed classes

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
 let searchButton = document.querySelector('.search-link');
 let searchFormContainer = document.querySelector('.search-form-container');
+
 searchButton.addEventListener('click', function () {
-    toggle(searchButton, 'search-link--clicked')
-    toggle(searchFormContainer, 'search-form-container--show')
+    toggleClass(searchButton, 'search-link--clicked')
+    toggleClass(searchFormContainer, 'search-form-container--show')
 });
+
+let mobileMenu = document.querySelector(".mobile-menu")
+let mobileMenuButton = document.querySelector(".mobile-menu__button")
 
 document.addEventListener("click", function(event) {
     if (event.target.closest(".mobile-menu__button")) {
-        toggle(".mobile-menu", "mobile-menu__visible")
-        toggle(".mobile-menu__button", "mobile-menu__opened")
-        toggle(".navbar__logo", "mobile-menu__opened")
-        toggle("body", "constrained")
+        toggleClass(mobileMenu, "mobile-menu--visible")
+        toggleClass(mobileMenuButton, "js-slide--right")
+        toggleClass(".navbar__logo", "js-slide--right")
+        toggleClass("body", "constrained")
         return;
     }
-    if (document.querySelector(".mobile-menu").classList.contains("mobile-menu__visible")) {
+    if (mobileMenu.classList.contains("mobile-menu--visible")) {
     if (event.target.closest(".mobile-menu")) {
         return;
         
     }
-    toggle(".mobile-menu", "mobile-menu__visible")
-    toggle(".mobile-menu__button", "mobile-menu__opened")
-    toggle(".navbar__logo", "mobile-menu__opened")
-    toggle("body", "constrained")
+    removeClass(mobileMenu, "mobile-menu--visible")
+    removeClass(mobileMenuButton, "js-slide--right")
+    removeClass(".navbar__logo", "js-slide--right")
+    removeClass("body", "constrained")
     }
 
 }) 
 
-
-function toggle(selector, className) {
+function addClass(selector,className) {
+    let findedSelector = typeof selector == 'object' ? selector : document.querySelector(selector);
+    findedSelector.classList.add(className);
+}
+function removeClass(selector,className) {
+    let findedSelector = typeof selector == 'object' ? selector : document.querySelector(selector);
+    findedSelector.classList.remove(className);
+}
+function toggleClass(selector, className) {
     let findedSelector = typeof selector == 'object' ? selector : document.querySelector(selector);
     findedSelector.classList.toggle(className);
 }
