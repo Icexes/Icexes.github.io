73ae887020a85e79cbe28632deca702ca48a17f2
Denis <suboch.denny@gmail.com>
2020-01-13 16:11:45 +0300

    added copyrights component

---
+++ src/my-app/src/components/Footer/Copyrights/Copyrights.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Copyrights() {
+    return (
+        <div className="copyrights">
+            © 2019  <a href="#">Pivotal Software</a> , Inc. All Rights Reserverd. <a href="#">Terms of Use</a> • <a href="#">Privacy</a> •  <a href="#">Trademark Guidelines</a>
+        </div>
+    )
+}
--- src/my-app/src/components/Footer/Footer.js
+++ src/my-app/src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FooterMenu from './FooterMenu/FooterMenu'
+import Copyrights from './Copyrights/Copyrights'
 export default function Footer() {
     return (
         <footer className="footer">
             <div className="container-wrap">
                 <FooterMenu />
+                <Copyrights /> 
             </div>
         </footer>
     )
