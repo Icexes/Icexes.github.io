b0b3d6a3b06808c278ec9e688eb3bccd2412345d
Denis <suboch.denny@gmail.com>
2020-01-13 10:02:05 +0300

    started to make components

--- src/my-app/src/components/Header/Header.js
+++ src/my-app/src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import MobileSearchField from './MobileSearchField'
+import MobileSearchField from './MobileSearchField/MobileSearchField'
 export default function Header() {
     return (
         <header className="header">
             <div className = "navbar-wrap">
                 <div className = "container-wrap">
                     <div className="navbar">
-                        <MobileSearchField />
+                        
                     </div>
                 </div>
             </div>
---
+++ src/my-app/src/components/Header/MobileMenu/MobileMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MobileSearchField from './MobileSearchField/MobileSearchField'
+export default function MobileMenu() {
+    return (      
+            <div className = "mobile-menu">
+                <div className = "container-wrap">
+                        <MobileSearchField />
+                    
+                </div>
+            </div>      
+    )
+}
