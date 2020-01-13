48f35219f699a0ac8726f978bfb4b3fdf522da9e
Denis <suboch.denny@gmail.com>
2020-01-13 13:46:07 +0300

    added DesktopSearchField file

---
+++ src/my-app/src/components/Header/DesktopSearchField/DesktopSearchField.js
@@ -0,0 +1,16 @@
+import React from 'react'
+export default function DesktopSearchField() {
+    return (
+        <div className="search-form-container">
+            <div className="container-wrap">
+                <form className="form-search" action="" method="GET">
+                    <input className="form-search__input search-field--js" type="text"
+                        placeholder="Search for documentation, guides, and posts..." value="" />
+                        <button className="form-search__submit search-button button" type="submit">
+                            <i className="fas fa-search search-button__icon"></i>                        
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
