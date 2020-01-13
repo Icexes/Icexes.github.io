85ce5d2de15a3dc2bd9c845c829245c06c117261
Denis <suboch.denny@gmail.com>
2020-01-13 11:01:36 +0300

    added id for li elements

--- src/my-app/src/components/Header/MobileMenu/NavigationItemList/NavigationItemList.js
+++ src/my-app/src/components/Header/MobileMenu/NavigationItemList/NavigationItemList.js
@@ -1,30 +1,35 @@
 import React from 'react'
 export default function NavigationItemList() {
     const itemListData = [
-        {
+        {   
+            id="1",
             href: "#",
             value: "Home"
         },
         {
+            id="2",
             href: "#",
             value: "Projects",
         },
         {
+            id="3",
             href: "#",
             value: "Guides"
         },
         {
+            id="4",
             href: "#",
             value: "Blog"
         }
         ,
         {
+            id="5",
             href: "#",
             value: "Training & Certification"
         }
     ]
     const itemList = itemListData.map((element) =>
-        <li className="mobile-menu__navbar-link">
+        <li key={element.id} className="mobile-menu__navbar-link">
             <a href={element.href}>{element.value}
                 <i className="fa fa-angle-right"></i></a>
         </li>
