6a094044d10cb2d2280dc678ad133c94b05f25bc
Denis <suboch.denny@gmail.com>
2020-01-13 11:07:01 +0300

    added MobileMenu in Header

--- src/my-app/src/components/Header/Header.js
+++ src/my-app/src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import MobileSearchField from './MobileSearchField/MobileSearchField'
+import MobileMenu from './MobileMenu/MobileMenu'
 export default function Header() {
     return (
         <header className="header">
             <div className="navbar-wrap">
                 <div className="container-wrap">
                     <div className="navbar">
-
+                        <MobileMenu />
                     </div>
                 </div>
             </div>
