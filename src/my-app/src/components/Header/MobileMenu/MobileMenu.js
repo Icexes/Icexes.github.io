9592c9d5beafd67688ad85ecc7a5933b31166b71
Denis <suboch.denny@gmail.com>
2020-01-13 11:05:07 +0300

    added element in MobileMenu

--- src/my-app/src/components/Header/MobileMenu/MobileMenu.js
+++ src/my-app/src/components/Header/MobileMenu/MobileMenu.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import MobileSearchField from './MobileSearchField/MobileSearchField'
+import NavigationItemList from './NavigationItemList/NavigationItemList'
 export default function MobileMenu() {
     return (
         <div className="mobile-menu">
             <div className="container-wrap">
                 <MobileSearchField />
-
+                <NavigationItemList />               
             </div>
         </div>
     )
