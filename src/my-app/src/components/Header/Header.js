0999154997ae36484791e84101b3fa4b6f12a9d7
Denis <suboch.denny@gmail.com>
2020-01-13 11:32:12 +0300

    created BurgerButton  && renamed in css mobMenuBtn

--- src/my-app/src/components/Header/Header.js
+++ src/my-app/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import MobileMenu from './MobileMenu/MobileMenu'
+import BurgerButton from './MobileMenu/BurgerButton/BurgerButton'
 export default function Header() {
     return (
         <header className="header">
@@ -7,6 +8,8 @@ export default function Header() {
                 <div className="container-wrap">
                     <div className="navbar">
                         <MobileMenu />
+                        <BurgerButton />
+                        
                     </div>
                 </div>
             </div>
---
+++ src/my-app/src/components/Header/MobileMenu/BurgerButton/BurgerButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BurgerButton() {
+    return (
+            <button className = "burger-button">
+                <span className="burger-button__line"></span>
+                <span className="burger-button__line"></span>
+                <span className="burger-button__line"></span>
+            </button>
+    )
+}
