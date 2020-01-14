2a97282276d93ecaa0a69c5c41464c0912e68849
Denis <suboch.denny@gmail.com>
2020-01-14 10:41:26 +0300

    added href in arrays

--- src/my-app/src/components/DataFiles/CardsDataArrays.js
+++ src/my-app/src/components/DataFiles/CardsDataArrays.js
@@ -1,109 +1,131 @@
 export const mainProjectCardsDataArray = [{
+    href: "#",
     src: "img/icons/spring-boot.png",
     title: "Spring boot",
     description: "Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible."
 },
 {
+    href: "#",
     src: "img/icons/spring-framework.png",
     title: "Spring Framework",
     description: "Provides core support for dependency infection, transaction management, webapps, data access, messaging and more."
 },
 {
+    href: "#",
     src: "img/icons/spring-cloud-data-flow.png",
     title: "Spring Cloud Data Flow",
     description: "An orchestration service for composable data microservice applications on modern runtimes."
 },
 {
+    href: "#",
     src: "img/icons/spring-cloud.png",
     title: "Spring Cloud",
     description: "An orchestration service for composable data microservice applications on modern runtimes."
 },
 {
+    href: "#",
     src: "img/icons/spring-data.png",
     title: "Spring Data",
     description: "An orchestration service for composable data microservice applications on modern runtimes."
 },
 {
+    href: "#",
     src: "img/icons/spring-integration.png",
     title: "Spring Integration",
     description: "An orchestration service for composable data microservice applications on modern runtimes."
 },
 {
+    href: "#",
     src: "img/icons/spring-batch.png",
     title: "Spring Batch",
     description: "Simplifies and optimizes the work of processing high-volume batch operations."
 },
 {
+    href: "#",
     src: "img/icons/spring-security.png",
     title: "Spring Security",
     description: "Protects your application with comprehensive and extensible authentication and authorization support."
 },
 {
+    href: "#",
     src: "img/icons/spring-hateoas.png",
     title: "Spring Hateoas",
     description: "Simplifies creating REST representations that follow the HATEOAS principle."
 },
 {
+    href: "#",
     src: "img/icons/spring-rest-docs.png",
     title: "Spring Rest Docs",
     description: "Document RESTful services by combining hand-written documentation with auto-generated snippets produced with Spring MVC searchFieldsArray or REST Assured."
 },
 {
+    href: "#",
     src: "img/icons/spring-amqp.png",
     title: "Spring Amqp",
     description: "Applies core Spring concepts to the development of AMQP-based messaging solutions."
 },
 {
+    href: "#",
     src: "img/icons/spring-mobile.png",
     title: "Spring Mobile",
     description: "Simplifies the development of mobile web apps through device detection and progressive rendering options."
 },
 {
+    href: "#",
     src: "img/icons/spring-for-android.png",
     title: "Spring For Android",
     description: "Provides key Spring components for use in developing Android applications."
 },
 {
+    href: "#",
     src: "img/icons/spring-web-flow.png",
     title: "Spring Web Flow",
     description: "Supports building web applications with controlled navigation such as checking in for a flight or applying for a loan."
 },
 {
+    href: "#",
     src: "img/icons/spring-web-services.png",
     title: "Spring Web Services",
     description: "Facilitates the development of contract-first SOAP web services."
 },
 {
+    href: "#",
     src: "img/icons/spring-ldap.png",
     title: "Spring Ldap",
     description: "Simplifies the development of applications using LDAP using Spring's familiar template-based approach."
 },
 {
+    href: "#",
     src: "img/icons/spring-session.png",
     title: "Spring Session",
     description: "Spring Session provides an API and implementations for managing a user’s session information."
 },
 {
+    href: "#",
     src: "img/icons/spring-shell.png",
     title: "Spring Shell",
     description: "Provides a powerful foundation for building command-line apps using a Spring-based programming model."
 },
 {
+    href: "#",
     src: "img/icons/spring-flo.png",
     title: "Spring Flo",
     description: "A JavaScript library that offers a basic embeddable HTML5 visual builder for pipelines and simple graphs."
 },
 {
+    href: "#",
     src: "img/icons/spring-kafka.png",
     title: "Spring Kafka",
     description: "Provides Familiar Spring Abstractions for Apache Kafka."
 },
 {
+    href: "#",
     src: "img/icons/spring-statemachine.png",
     title: "Spring Statemachine",
     description: "A framework for application developers to use state machine concepts with Spring applications "
 },
 {
+    href: "#",
     src: "img/icons/spring-io-platform.png",
     title: "Spring Io Platform",
     description: "Provides a cohesive, versioned platform for building modernapplications. It is a modular, enterprise-grade distribution that delivers a curated set of dependencies."
@@ -111,10 +133,12 @@ export const mainProjectCardsDataArray = [{
 ]
 export const communityProjectsDataArray = [
     {
+        href: "#",
         title: "Spring Roo",
         description: "Makes it fast and easy to build full Java applications in minutes."
     },
     {
+        href: "#",
         title: "Spring Scala",
         description: "Brings the power and expressiveness of Scala together with the productivity and deep ecosystem of Spring."
     },
@@ -122,14 +146,17 @@ export const communityProjectsDataArray = [
 
 export const projectInTheAtticDataArray = [
     {
+        href: "#",
         title: "REST Shell",
         description: "Makes writing and testing RESTful applications easier with CLI-based resource discovery and interaction."
     },
     {
+        href: "#",
         title: "Spring XD",
         description: "Simplifies the development of big data applications by addressing ingestion, analytics, batch jobs and data export."
     },
     {
+        href: "#",
         title: "Spring Social",
         description: "Easily connects your applications with third-party APIs such as Facebook, Twitter, LinkedIn, and more."
     },
