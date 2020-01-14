dedf8c778f3605392d6dfcae0178c6eb0bc757ed
Denis <suboch.denny@gmail.com>
2020-01-14 14:51:56 +0300

    added css for footer elements

--- src/my-app/src/components/Main/MainContent/Cards/Card/Card.js
+++ src/my-app/src/components/Main/MainContent/Cards/Card/Card.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import './Card.css'
 export default function Card(props) {
-    
+
     const makeCard = () => {
     }
     return (
-        
-        <a className = {props.small ? "card card--height-s" : "card"} href={props.cardData.href}>
-        {!props.small && (<div className="card__icon-container">
-            <div className = {props.cardData.src}></div>
-        </div>)}
-                <p className="card__title">{props.cardData.title}</p>
-                <p className="card__description">{props.cardData.description}</p>
-                
+
+        <a className={props.small ? "card card--height-s" : "card"} href={props.cardData.href}>
+            {!props.small && (<div className="card__icon-container">
+                <div className={props.cardData.src}></div>
+            </div>)}
+            <p className="card__title">{props.cardData.title}</p>
+            <p className="card__description">{props.cardData.description}</p>
+
         </a>
     )
 }
