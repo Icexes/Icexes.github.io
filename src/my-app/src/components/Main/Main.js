1cce81270af41db71cff67f44a01e3365b713966
Denis <suboch.denny@gmail.com>
2020-01-14 15:24:46 +0300

    added css for header elements

--- src/my-app/src/components/Main/Main.js
+++ src/my-app/src/components/Main/Main.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Main.css'
+import MainContent from './MainContent/MainContent'
 export default function Main() {
     return (
         <div >
-
+            <MainContent/>
         </div>
     )
 }
