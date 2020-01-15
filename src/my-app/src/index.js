588f6242cb97dea70da4f4734327d1f16bba641e
Denis <suboch.denny@gmail.com>
2020-01-15 11:51:32 +0300

    deleted styles.css, changed styles in index.css

--- src/my-app/src/index.js
+++ src/my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './styles.css';
+
 
 import './font-awesome/css/all.min.css'
 import Header from "./components/Header/Header"
