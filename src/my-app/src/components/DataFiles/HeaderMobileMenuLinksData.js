c83ab37e71ec6127b41e3f61aad2196ce404ddcd
Denis <suboch.denny@gmail.com>
2020-01-16 14:55:24 +0300

    added data file for mobile menu

---
+++ src/my-app/src/components/DataFiles/HeaderMobileMenuLinksData.js
@@ -0,0 +1,32 @@
+const menuLinksData =
+    [
+        {
+            id: "1",
+            href: "#",
+            value: "Home",
+        },
+
+        {
+            id: "2",
+            href: "#",
+            value: "Projects",
+        },
+        {
+            id: "3",
+            href: "#",
+            value: "Guides"
+        },
+        {
+            id: "4",
+            href: "#",
+            value: "Blog"
+        }
+        ,
+        {
+            id: "5",
+            href: "#",
+            value: "Training & Certification"
+        }
+    ]
+export default menuLinksData
+
