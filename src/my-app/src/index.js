816b24db8cf096e090c18809bac5f0801ccaa71c
Denis <suboch.denny@gmail.com>
2020-01-17 10:19:05 +0300

    trying to make search on the main arctice

--- src/my-app/src/index.js
+++ src/my-app/src/index.js
@@ -1,59 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-
 import './font-awesome/css/all.min.css'
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import Main from "./components/Main/Main"
-import {mainProjectCardsDataArray} from './components/DataFiles/CardsDataArrays'
-
-
+import { mainProjectCardsDataArray } from './components/DataFiles/CardsDataArrays'
 
 
 class MainStuff extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      mainProjectCardsDataArray : mainProjectCardsDataArray,
+      mainProjectCardsDataArray: mainProjectCardsDataArray,
       searchQuery: ''
     }
   }
 
-   handleInputChange = async (e) => {
-   await this.setState({searchQuery: e.target.value})
+  handleInputChange = async (e) => {
+    await this.setState({ searchQuery: e.target.value })
     if (this.state.searchQuery) {
-      this.filterCards(mainProjectCardsDataArray)
+      await this.setState({ mainProjectCardsDataArray: this.filterCards(mainProjectCardsDataArray) })
+      
     }
   }
 
-  filterCards = (arrays) =>{
-    if (this.state) {
-      let newArr = arrays.map(arr =>{
-        if(arr.description.indexOf(this.state)) {
-          alert(this.state)
-          alert(arr.id)
-          return arr
-        }
-
-      })
-     
-    }
-
-  }
-    render() {
-      return (
-        <>
-        <Header handleInputChange={this.handleInputChange}/>
-        <Main />
-        <Footer />
+  filterCards = (arrays) => {
     
+    let newArr = arrays.filter(arr => {
+      if (arr.description.indexOf(this.state.searchQuery)!=-1) {
+       alert(arr.id)       
+        return arr
+      }
+    }
+    )
+    console.log(newArr)
+  }
+
+  render() {
+    return (
+      <>
+        <Header handleInputChange={this.handleInputChange} />
+        <Main mainProjectCardsDataArray={this.state.mainProjectCardsDataArray}/>
+        <Footer />
+
       </>
 
-      )
+    )
 
-    }
+  }
 }
 
 
@@ -61,8 +56,7 @@ class MainStuff extends React.Component {
 // ========================================
 ReactDOM.render(
   <>
-<MainStuff/>
-
+    <MainStuff />
   </>,
   document.getElementById('root')
 );
