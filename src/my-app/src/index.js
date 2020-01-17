bfa2f0668383266e4c68e04d1fbd54ce462e7e0f
Denis <suboch.denny@gmail.com>
2020-01-17 17:51:38 +0300

    trying to make backlight

--- src/my-app/src/index.js
+++ src/my-app/src/index.js
@@ -12,19 +12,20 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      mainProjectCardsDataArray: mainProjectCardsDataArray,
+      mainProjectCardsDataArray: mainProjectCardsDataArray.slice(),
       searchQuery: '',
       isNotFound: false,
     }
   }
 
   handleSearchInputChange = async (e) => {
-    
+    console.log(this.state.mainProjectCardsDataArray,"STATE")
+    console.log(mainProjectCardsDataArray,"USUAL")
     await this.setState({ searchQuery: e.target.value })
     console.log("YES", this.state.searchQuery.length)
     if (this.state.searchQuery) {
       console.log("WHY")
-      await this.setState({ mainProjectCardsDataArray: this.filterCards(mainProjectCardsDataArray) })
+      await this.setState({ mainProjectCardsDataArray: this.filterCards(mainProjectCardsDataArray.slice()) })
       if (this.state.mainProjectCardsDataArray.length) {
         await this.setState({isNotFound: false})
       }
@@ -36,26 +37,26 @@ class App extends React.Component {
       console.log("HI")
     //  console.log(mainProjectCardsDataArray)
       await this.setState({isNotFound: false})
-      await this.setState({ mainProjectCardsDataArray: mainProjectCardsDataArray })
-      console.log(this.state.mainProjectCardsDataArray)
+      await this.setState({ mainProjectCardsDataArray: mainProjectCardsDataArray.slice() })
+      
     }
   }
   
-  //addMark = (str, pos, length) => str.slice(0, pos) + '<mark>' + str.slice(pos, pos + length) + '</mark>' + str.slice(pos + length);
+  addMark = (str, pos, length) => str.slice(0, pos) + '<mark>' + str.slice(pos, pos + length) + '</mark>' + str.slice(pos + length);
   
   
    filterCards = (arrays) => {
     
     let newArr = arrays.filter(arr => {
       
-      if (arr.description.toLowerCase().indexOf(this.state.searchQuery.toLowerCase())!==-1 || arr.title.toLowerCase().indexOf(this.state.searchQuery.toLowerCase())!==-1) {
+      if (arr.description.indexOf(this.state.searchQuery)!==-1) { //|| arr.title.toLowerCase().indexOf(this.state.searchQuery.toLowerCase())!==-1
         return arr
       }
     }
     )
-   // newArr.forEach(elem => {
-   //   elem.description = this.addMark(elem.description, elem.description.indexOf(this.state.searchQuery),this.state.searchQuery.length)
-   // })
+    newArr.forEach(elem => {
+     elem.description = this.addMark(elem.description, elem.description.indexOf(this.state.searchQuery),this.state.searchQuery.length)
+    })
     
     return newArr
   }
