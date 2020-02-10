a96b0c3409f61cc0d9772482c2c0d748d506439d
Denis <suboch.denny@gmail.com>
2020-02-10 12:09:14 +0300

    feat: added 3 actions and 2 action creators

---
+++ src/my-app/src/actions/actions.js
@@ -0,0 +1,6 @@
+const ADD_SEARCH_QUERY = 'ADD_SEARCH_QUERY'
+const TOGGLE_SEARCH_FIELD = 'TOGGLE_SEARCH_FIELD'
+const TOGGLE_MOBILE_MENU = 'TOGGLE_MOBILE_MENU'
+
+export const toggleMobileMenu = () => {type: TOGGLE_MOBILE_MENU}
+export const toggleSearchField = () => {type: TOGGLE_SEARCH_FIELD}
