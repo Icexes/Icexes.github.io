2d4ea748cdb618d4a3a754cc39abc2c38671a01b
Denis <suboch.denny@gmail.com>
2020-02-10 12:32:17 +0300

    feat: add reducer

---
+++ src/my-app/src/reducers/reducers.js
@@ -0,0 +1,17 @@
+import {TOGGLE_SEARCH_FIELD, TOGGLE_MOBILE_MENU} from '../actions/actions'
+
+const initialState = {
+    searchFieldIsOpen: false,
+    mobileMenuIsOpen: false
+}
+
+function app(state=initialState, action) {
+    switch(action.type) {
+        case TOGGLE_SEARCH_FIELD :
+            return Object.assign({}, state, {searchFieldIsOpen : !state.searchFieldIsOpen})
+        case TOGGLE_MOBILE_MENU :
+            return Object.assign({}, state, {mobileMenuIsOpen: !state.mobileMenuIsOpen})
+        default:
+            return state
+    }
+} 
